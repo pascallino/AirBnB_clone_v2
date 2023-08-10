3c90303c99f120db671ddad2b07af52f89bb76a2
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-09 23:42:17 -0700

    testfilestaorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+"""unittest Module for FileStorage class"""
+
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.engine.file_storage import FileStorage
+import datetime
+import json
+import os
+import models
+import unittest
+
+
+class TestFileStorage_save(unittest.TestCase):
+    """ test save method in Filestorage class """
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "pascal")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("pascal", "file.json")
+        except IOError:
+            pass
+
+    def clearStorage(self):
+        """ clear the file contents """
+        FileStorage._FileStorage__objects = {}
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("pascal", "file.json")
+        except IOError:
+            pass
+
+    def test_save_bypassing_None_parameter(self):
+        """ test_save_bypassing_None_parameter """
+        with self.assertRaises(TypeError):
+            models.storage.save(None)
+
+    def test_save_method(self):
+        """ test save function in filestorage class"""
+        bmodel = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+        models.storage.new(bmodel)
+        models.storage.new(user)
+        models.storage.new(state)
+        models.storage.new(place)
+        models.storage.new(city)
+        models.storage.new(amenity)
+        models.storage.new(review)
+        models.storage.save()
+        with open("file.json", 'r') as file:
+            getllobjs = file.read()
+        self.assertIn("BaseModel." + bmodel.id, getllobjs)
+        self.assertIn("User." + user.id, getllobjs)
+        self.assertIn("State." + state.id, getllobjs)
+        self.assertIn("Place." + place.id, getllobjs)
+        self.assertIn("City." + city.id, getllobjs)
+        self.assertIn("Amenity." + amenity.id, getllobjs)
+        self.assertIn("Review." + review.id, getllobjs)
+
+
+class TestFileStorage_reload(unittest.TestCase):
+    """ test reload method in Filestorage class """
+    def test_reload(self):
+        """ test_reload function"""
+        bmodel = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+        models.storage.new(bmodel)
+        models.storage.new(user)
+        models.storage.new(state)
+        models.storage.new(place)
+        models.storage.new(city)
+        models.storage.new(amenity)
+        models.storage.new(review)
+        models.storage.save()
+        models.storage.reload()
+        getllobjs = FileStorage._FileStorage__objects
+        self.assertIn("BaseModel." + bmodel.id, getllobjs)
+        self.assertIn("User." + user.id, getllobjs)
+        self.assertIn("State." + state.id, getllobjs)
+        self.assertIn("Place." + place.id, getllobjs)
+        self.assertIn("City." + city.id, getllobjs)
+        self.assertIn("Amenity." + amenity.id, getllobjs)
+        self.assertIn("Review." + review.id, getllobjs)
+
+    def test_reload_bypassing_None_parameter(self):
+        """ test_reload_bypassing_None_parameter """
+        with self.assertRaises(TypeError):
+            models.storage.reload(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
