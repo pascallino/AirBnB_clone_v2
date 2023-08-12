ca79760251c655995227abc9d386684f9a61fe06
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-12 07:21:20 -0700

    filestoreage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,6 +16,7 @@ class FileStorage:
     """ File Storage class to store the
     to save the obj dict to json string and retrieve
     as Object"""
+
     __file_path = "file.json"
     __objects = {}
 
@@ -39,11 +40,14 @@ class FileStorage:
     def reload(self):
         """ deserializes back to python object
         do nothing if the file doesnt exist"""
-        if not os.path.isfile(FileStorage.__file_path):
+        # if not os.path.isfile(FileStorage.__file_path):
+        # return
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                pyobj = json.load(file)
+                for cl in pyobj.values():
+                    clName = cl["__class__"]
+                    del cl["__class__"]
+                    self.new(eval(clName)(**cl))
+        except FileNotFoundError:
             return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-            pyobj = json.load(file)
-            for cl in pyobj.values():
-                clName = cl["__class__"]
-                del cl["__class__"]
-                self.new(eval(clName)(**cl))
