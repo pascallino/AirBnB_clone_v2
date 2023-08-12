37e850481d90816dc32b60ee076e5a83fcce42bc
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-12 08:03:01 -0700

    filestore

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,8 @@
 """ initialize the filestorage class to always
 create an instance of its class that will be
 throughout the application """
-
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
