228370550fd2657b84c7cc5327957c220bad6fb5
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-12 08:46:27 -0700

    unitestfilestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -118,7 +118,7 @@ class TestFileStorage_reload(unittest.TestCase):
             pass
 
     def test_reload(self):
-        """ test_reload function"""
+        """ test_reload function to see if it works"""
         bmodel = BaseModel()
         user = User()
         state = State()
@@ -243,21 +243,6 @@ class TestFileStorage_new(unittest.TestCase):
     def test_storage_type(self):
         """ test storage type"""
         self.assertEqual(type(models.storage), FileStorage)
-
-
-class test_fileStorage(unittest.TestCase):
-    """Test FileStorage Class"""
-    def test_instances(self):
-        """chequeamos instantation"""
-        obj = FileStorage()
-        self.assertIsInstance(obj, FileStorage)
-
-    def test_docs(self):
-        """Test docstrings"""
-        self.assertIsNotNone(FileStorage.all)
-        self.assertIsNotNone(FileStorage.new)
-        self.assertIsNotNone(FileStorage.save)
-        self.assertIsNotNone(FileStorage.reload)
 
 
 if __name__ == "__main__":
