a7a415563d3aa5adbe0ffd69b2a35c28ccfe64e7
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-12 04:39:26 -0700

    pycodestyle

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/env/python3
-""" initialize the filestorage class to always 
-create an instance of its class that will be 
+""" initialize the filestorage class to always
+create an instance of its class that will be
 throughout the application """
 
 from models.engine.file_storage import FileStorage
