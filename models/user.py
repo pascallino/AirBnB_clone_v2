01d7558ed33fd765b0adfaadfcc0f77a332ec2c5
pascallino <luvpascal.ojukwu@yahoo.com>
2023-09-17 02:45:13 -0700

    models/user.py

--- models/user.py
+++ models/user.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """  class User that inherits from BaseModel:"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import *
+from sqlalchemy.orm import *
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """Public class attributes:
 email: string - empty string
 password: string - empty string
 first_name: string - empty string
 last_name: string - empty string"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
