8b94b8fb51adaa2a541d12621c94379faf47baa0
pascallino <luvpascal.ojukwu@yahoo.com>
2023-09-18 02:21:16 -0700

    done

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,6 @@
 create an instance of its class that will be
 throughout the application """
 from os import getenv
-from models.engine.file_storage import FileStorage
 from models import *
 from models.user import User
 from models.state import State
@@ -15,7 +14,9 @@ from models.review import Review
 from models.base_model import BaseModel, Base
 
 if getenv('HBNB_TYPE_STORAGE') == "db":
-    storage = FileStorage()
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
